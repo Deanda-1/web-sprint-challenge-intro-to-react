f78a754b5b4c5f9932789571900699e6b5a83936
DJ1 <jimmynoble604@gmail.com>
2023-03-01 08:09:20 -0500

    character.js and some of the app.js complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const App = () => {
+  const [character, setCharacter] = useState([])
+  useEffect(() => {
+    axios.get(`https://swapi.dev/api/people/`)
+    .then(res => {
+      console.log(res.data);
+      setCharacter(res.data);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }, [])
+
+  useEffect (() => {
+    axios.get(`https://swapi.dev/api/people/`)
+    .then(res => {
+      console.log(res.data)
+      character(res.data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  })
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -12,6 +35,7 @@ const App = () => {
     <div className="App">
       <h1 className="Header">Characters</h1>
     </div>
+    
   );
 }
 
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,6 +1,5 @@
 // src/mocks/handlers.js
 import { rest } from "msw";
-
 export const handlers = [
   rest.get("https://swapi.dev/api/people/", (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(data));
